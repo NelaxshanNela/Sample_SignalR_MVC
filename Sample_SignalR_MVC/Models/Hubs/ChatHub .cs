44a1f0e072bea75271b09bba9063c27be81fba84
Nelaxshan UCA <nelaxshan@unicornconnectedapps.com>
2025-03-20 16:40:06 +0530

    Add project files.

---
+++ Sample_SignalR_MVC/Models/Hubs/ChatHub .cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Sample_SignalR_MVC.Models.Hubs
+{
+    public class ChatHub : Hub
+    {
+        // Method that clients can call to send messages
+        public async Task SendMessage(string user, string message)
+        {
+            // Sends message to all connected clients
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
